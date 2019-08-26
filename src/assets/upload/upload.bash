aa928ab60a165d9519465c90be382d102409d70f
Paulius Kuzmickas <p.kuzmickas@gmail.com>
2019-08-26 20:39:59 +0100

    Almost Done

--- src/assets/upload/upload.bash
+++ src/assets/upload/upload.bash
@@ -5,9 +5,16 @@ printf '\n'
 echo "( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)"
 printf '\n'
 for filename in ./*.jpg; do
+    if [ ${filename:2:4} == "min-" ]
+      then
+        continue
+    fi
 
     while [[ "${ans}" > 4 ]] || [[ "${ans}" < 0 ]]; do
-    echo "What category does $filename go to?"
+    printf '\n'
+    echo "MAKE SURE YOU HAVE THE MINIFIED FILE AS WELL ! ! ! !"
+    printf '\n'
+    echo "What category does ${filename:2} go to?"
     echo "0 - People"
     echo "1 - Compositions"
     echo "2 - Food"
@@ -18,11 +25,12 @@ for filename in ./*.jpg; do
 
     categories=(people compositions food landscapes other)
 
-    echo "Moving file to designated folder..."
+    echo "Moving main file and minified file to the designated folder..."
     mv "$filename" ../pics/${categories[ans]}
+    mv "${filename:0:2}min-${filename:2}" ../pics/${categories[ans]}
     echo "Adding filename to order.csv..."
     echo "${filename:2}" >> ../pics/${categories[ans]}/order.csv
-    echo "Done!"
+    echo "Done! ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)"
     printf '\n'
     ans=-1;
 
