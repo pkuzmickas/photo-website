927716d121cac32595dd62842872713c359aa332
Paulius Kuzmickas <p.kuzmickas@gmail.com>
2019-10-13 12:42:28 +0100

    Changed layout

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -23,7 +23,12 @@ export class PortfolioPageComponent implements AfterViewInit {
   modalOpened = false;
   modalPic;
 
-  columns = [];
+  stdCols = 5;
+  compCols = 3;
+
+  data = [];
+  fData = []; // formatted
+
   ngAfterViewInit(): void {
     const element = document.getElementById('first') as HTMLElement;
     this.prevCatTarget = element;
@@ -31,22 +36,28 @@ export class PortfolioPageComponent implements AfterViewInit {
   }
 
   constructor(private http: HttpClient) {
-    this.loadOrder();
+    this.loadOrder(this.stdCols);
 
   }
   @HostListener('window:scroll', ['$event'])
-    handleScroll() {
-        const windowScroll = window.pageYOffset;
-        if (windowScroll >= this.menuPosition) {
-            this.sticky = true;
-        } else {
-            this.sticky = false;
-        }
+  handleScroll() {
+    const windowScroll = window.pageYOffset;
+    if (windowScroll >= this.menuPosition) {
+      this.sticky = true;
+    } else {
+      this.sticky = false;
     }
+  }
   changeCat(cat, event) {
-    console.log(this.prevCatTarget)
+    let target = document.getElementById('scrollTarget');
+    target.scrollIntoView();
     this.curCategory = this.categories[cat];
-    this.loadOrder();
+    if (cat === 1) { // compositions
+      this.loadOrder(this.compCols);
+    } else {
+      this.loadOrder(this.stdCols);
+    }
+
     event.target.classList.add('active'); // To ADD
     if (this.prevCatTarget) {
       this.prevCatTarget.classList.remove('active');
@@ -65,25 +76,35 @@ export class PortfolioPageComponent implements AfterViewInit {
     target.scrollIntoView();
   }
 
-  loadOrder() {
+  loadOrder(cols) {
     this.http.get('assets/pics/' + this.curCategory + '/order.csv', { responseType: 'text' })
       .subscribe(
         data => {
-          let finalData = [];
+          this.data = [];
+          this.fData = [];
           data.split('\n').forEach(element => {
-            finalData.push(element.split(';'));
+            this.data.push(element.split(';'));
           });
-          this.columns = [];
-          for (let i = 0; i < finalData[0].length; i++) {
-            let col = [];
-            for (let j = 0; j < finalData.length; j++) {
-              if (finalData[j][i]) {
-                col.push(finalData[j][i]);
+          console.log(this.data);
+          if (this.curCategory !== this.categories[1]) {
+            for (let i = 0; i < this.data.length; i++) {
+              const row = this.data[i];
+              let firstRow = [];
+              let secondRow = [];
+              for (let j = 0; j < row.length; j++) {
+                const pic = row[j];
+                if (pic === '') {
+                  break;
+                }
+                if (j < 3) {
+                  firstRow.push(pic);
+                } else {
+                  secondRow.push(pic);
+                }
               }
+              this.fData.push([firstRow, secondRow]);
             }
-            this.columns.push(col);
           }
-          console.log(this.columns)
         },
         error => {
           console.log(error);
